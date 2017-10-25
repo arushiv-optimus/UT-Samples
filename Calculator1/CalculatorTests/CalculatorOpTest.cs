b57c6c12fb51022fc501e41da3fb66204c06190f
arushiv-optimus <arushi.vishwakarma@optimusinfo.com>
2017-10-25 11:43:26 +0530

    UT Sample Code...

---
+++ Calculator1/CalculatorTests/CalculatorOpTest.cs
@@ -0,0 +1,29 @@
+﻿using Calculator1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorTests
+{
+    [TestClass]
+   public class CalculatorOpTest
+    {
+        [TestMethod]
+        public void Test_Add()
+        {
+            CalculatorOp cal = new CalculatorOp();
+            int result = cal.Add(5, 4);
+            Assert.AreEqual(9, result);
+        }
+        [TestMethod]
+        public void Test_Mul()
+        {
+            CalculatorOp cal = new CalculatorOp();
+            int result = cal.Mul(5, 4);
+            Assert.AreEqual(20, result);
+        }
+    }
+}
