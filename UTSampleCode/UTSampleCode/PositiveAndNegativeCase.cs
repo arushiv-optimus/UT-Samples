5589fccbf9022feafdb815a7a896e0dc906bfb39
Smriti Singh <smriti.singh@optimusinfo.com>
2017-11-03 00:06:30 +0530

    Revert "Pull request"

---
+++ UTSampleCode/UTSampleCode/PositiveAndNegativeCase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTSampleCode
+{
+  public class PositiveAndNegativeCase
+    {
+            public bool IsPrime(int a)
+            {
+                bool result = true;
+                if (a >= 0)
+                {
+                    for (int i = 2; i <= a / 2; i++)
+                    {
+                        if (a % i == 0)
+                        {
+                            result = false;
+                            break;
+                        }
+                    }
+
+                   return result;
+                }
+                else
+                {
+                    return false;
+                }
+               
+            } 
+    }
+}
